e23678cc26c18dd34b3e121852a59eea61cb026f
Sehajini Jayakody <98893191+sehajini1@users.noreply.github.com>
2024-09-08 12:07:04 +0530

    removed unwanted things

--- src/pageComponents/login/LoginComponent.tsx
+++ src/pageComponents/login/LoginComponent.tsx
@@ -10,29 +10,16 @@ import {
 import { Input } from "../../@/components/ui/input";
 import { Label } from "../../@/components/ui/label";
 import { useState } from "react";
-//import { loginUser } from "../../store/authSlice";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  //const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
-
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   dispatch(loginUser({ email, password }));
-  // };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    navigate('/chatSection');
-    // try {
-    //   await dispatch(loginUser({ email, password })).unwrap();
-    //   navigate('/chatSection');
-    // } catch (err) {
-    //   console.log(err);
-    // }
+    navigate("/");
   };
 
   return (
@@ -46,63 +33,53 @@ export default function LoginForm() {
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit}>
-          <div className="grid gap-4">
-            <div className="grid gap-2 ">
-              <Label htmlFor="email" className="text-[#09090B]">
-                Email
-              </Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                className="border-inherit text-[#71717A] rounded-[0.5rem]"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password" className="text-[#09090B]">
-                  Password
+            <div className="grid gap-4">
+              <div className="grid gap-2 ">
+                <Label htmlFor="email" className="text-[#09090B]">
+                  Email
                 </Label>
-                <Link
-                  to="#"
-                  className="ml-auto inline-block text-sm underline text-[#09090B]"
-                >
-                  Forgot your password?
-                </Link>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  className="border-inherit text-[#71717A] rounded-[0.5rem]"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
-              <Input
-                id="password"
-                type="password"
-                required
-                className="border-inherit rounded-[0.5rem]"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <Button
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password" className="text-[#09090B]">
+                    Password
+                  </Label>
+                  <Link
+                    to="#"
+                    className="ml-auto inline-block text-sm underline text-[#09090B]"
+                  >
+                    Forgot your password?
+                  </Link>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  className="border-inherit rounded-[0.5rem]"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button
                 type="submit"
                 className="w-full rounded-[0.5rem] bg-[#18181B] text-[#FAFAFA] hover:bg-[#2F2F32] hover:text-[#E4E4E7]"
-                
-              >
-                
-              </Button>
-              
-            {/* <Button
-              variant="outline"
-              className="w-full rounded-[0.5rem] text-[#09090B] border-inherit"
-            >
-              Login with Google
-            </Button> */}
-          </div>
-          <div className="mt-4 text-center text-sm text-[#09090B]">
-            Don&apos;t have an account?{" "}
-            <Link to="/signup" className="underline">
-              Sign up
-            </Link>
-          </div>
+              ></Button>
+            </div>
+            <div className="mt-4 text-center text-sm text-[#09090B]">
+              Don&apos;t have an account?{" "}
+              <Link to="/signup" className="underline">
+                Sign up
+              </Link>
+            </div>
           </form>
         </CardContent>
       </Card>
